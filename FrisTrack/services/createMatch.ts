669122d09725fa79c2399b8e60f5ec26a0c3a0d3
Nathan <153100527+Akanaifu@users.noreply.github.com>
2025-12-09 22:18:37 +0100

    ajout service création match

---
+++ FrisTrack/services/createMatch.ts
@@ -0,0 +1,23 @@
+import apiClient from "./apiClient";
+import type { Localisation } from "./getLocalisation";
+
+export interface CreateMatchData {
+	title: string;
+	userTeamId: number;
+	opponentTeamId: number;
+	Localisation: Localisation;
+	date: string;
+	time?: string;
+	inOutdoor: "indoor" | "outdoor";
+	label: "finished" | "schedule";
+}
+
+export const createMatch = async (matchData: CreateMatchData) => {
+	try {
+		const response = await apiClient.post("/matches", matchData);
+		return response.data;
+	} catch (error) {
+		console.error("Error creating match:", error);
+		throw error;
+	}
+};
